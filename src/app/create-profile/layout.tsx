cf4d2ae9c506932fac563048c3b4887f3551bfb9
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-09-02 15:43:14 -0700

    center text

--- src/app/create-profile/layout.tsx
+++ src/app/create-profile/layout.tsx
@@ -4,9 +4,9 @@ export default function CreateProfileLayout({ children }: { children: React.Reac
 
 
   return (
-    <div className='w-full flex flex-col items-center justify-between min-h-screen bg-orange-400/80'>
-      <header className='w-full flex flex-col items-center justify-center bg-slate-50/75 p-4'>
-        <h1>Create Your profile</h1>
+    <div className='w-full flex flex-col items-center justify-start min-h-screen bg-orange-400/80'>
+      <header className='w-full max-w-10/12 text-center text-slate-800 p-1 my-2'>
+        <h1 className="text-4xl">Create Your profile</h1>
       </header>
       <main className="w-full">
         {children}
